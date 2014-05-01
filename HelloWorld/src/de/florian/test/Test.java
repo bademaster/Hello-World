7f3c5d61217b43596512786eacb699dc4d9e6125
Laptop <Laptop@Laptop-PC>
2014-05-01 14:59:48 +0200

    sfdsdfsdf

--- HelloWorld/src/de/florian/test/Test.java
+++ HelloWorld/src/de/florian/test/Test.java
@@ -1,5 +1,12 @@
-package de.florian.test;
 
+package de.florian.test;
+import java.io.File;
+import java.io.IOException;
+
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
 public class Test {
 
 	/**
@@ -10,5 +17,12 @@ public class Test {
 
 		// JSoup
 	}
-
+	
+	
+	public static org.jsoup.select.Elements getElements(File input) throws IOException {
+		String source = null;
+		org.jsoup.nodes.Document doc = Jsoup.parse(input, "utf-8", source);
+		org.jsoup.select.Elements elements = doc.select("div.fp_result");
+		return elements;
+	}
 }
